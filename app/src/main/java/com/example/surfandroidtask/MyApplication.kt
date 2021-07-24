af9863d132eecd7b6458573ca6318dd8365c6979
apoddubskaya <31957987+apoddubskaya@users.noreply.github.com>
2021-07-24 20:11:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/surfandroidtask/MyApplication.kt
@@ -0,0 +1,6 @@
+package com.example.surfandroidtask
+
+import android.app.Application
+
+class MyApplication: Application() {
+}
---
+++ app/src/main/java/com/example/surfandroidtask/ui/ListViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.surfandroidtask.ui
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel: ViewModel() {
+}
